20bbde40caec81cb335a7b43caa700a11b1c8f45
PavanPrasad473 <spavanprasad@gmail.com>
2019-06-05 16:03:06 +0530

    commit

---
+++ MainConsole/MainConsole/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainConsole
+{
+    class Ticket
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("This is Ticket class");
+            Console.ReadLine();
+        }
+    }
+}
